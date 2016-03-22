d00dd4a9e8809345fc1dfc4021326998c1039a45
mario8x <quangxnguyen@kms-technology.com>
2016-03-22 17:24:40 +0700

    Remove some comments
    just remove some comments and follow standard.

--- src/helpers/setup.js
+++ src/helpers/setup.js
@@ -12,7 +12,6 @@ exports.quitDriver = async (driver) => {
   if (driver != null) {
     try {
       await driver.quit()
-      //await exports.wait(30000)
     }
     catch (err) {
       debug.error('capabilities', err)
@@ -21,15 +20,13 @@ exports.quitDriver = async (driver) => {
 }
 exports.createDriver = async (desiredCaps) => {
   const driver = wd.promiseChainRemote(servers.getRemote())
-  /* eslint-disable no-console */
   driver.on('status', (info) => debug.log('helpers', info.cyan))
   driver.on('command', (meth, path, data) => debug.log('helpers',
   ' >', meth.yellow, path.grey, data || ''))
   driver.on('http', (meth, path, data) => debug.log('helpers',
   ' >', meth.magenta, path, (data || '').grey))
-  /* eslint-enable */
 
-  //Mocha doesn't show error data in its report
+  // Mocha doesn't show error data in its report
   try {
     await driver.init(desiredCaps)
   }
