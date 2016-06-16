525b14faf29872d7b507ceeb4954c66331dd1631
khanhdodang <khanhdo.pmp@gmail.com>
2016-06-16 11:47:16 +0700

    Add auto download Kobiton app

--- src/core/exec.js
+++ src/core/exec.js
@@ -1,26 +1,40 @@
 import BPromise from 'bluebird'
 import {debug} from '@kobiton/core-util'
-
-const exectAsync = BPromise.promisify(require('child_process').exec, {multiArgs: true})
-const options = {
-  encoding: 'utf8',
-  timeout: 0,
-  maxBuffer: 200 * 1024,
-  killSignal: 'SIGTERM',
-  cwd: null,
-  env: null
-}
+import {exec, spawn} from 'child_process'
 
 export async function killAdbProcess() {
   const cmd = (process.platform === 'win32') ? 'taskkill /f /im adb.exe' : 'killall adb'
-  const [stdout, stderr] = await exectAsync(cmd, options)
-  debug.log('exec', `stdout: ${stdout}`)
-  debug.log('exec', `stderr: ${stderr}`)
+  await _execCommandAsync(cmd)
 }
 
 export async function killKobitonApp() {
   const cmd = (process.platform === 'win32') ? 'taskkill /f /im Kobiton.exe' : 'killall Kobiton'
-  const [stdout, stderr] = await exectAsync(cmd, options)
-  debug.log('exec', `stdout: ${stdout}`)
-  debug.log('exec', `stderr: ${stderr}`)
+  await _execCommandAsync(cmd)
+}
+
+export async function executeCommand(cmd) {
+  return await _execCommandAsync(cmd)
+}
+
+async function _execCommandAsync(cmd, cb) {
+  debug.log('execCommandAsync', `cmd: ${cmd}`)
+  return await new BPromise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) reject(error.toString())
+      resolve(stdout.toString())
+    })
+  })
+}
+
+export async function executeCommandSpawn(cmd, args) {
+  debug.log('executeCommandSpawn', `cmd: ${cmd} , args: ${args}`)
+  return await new BPromise((resolve, reject) => {
+    const ls = spawn(cmd, args)
+    ls.stdout.on('data', (data) => {
+      if (data) resolve(data.toString())
+    })
+    ls.stderr.on('data', (data) => {
+      if (data) reject(data.toString())
+    })
+  })
 }
--- src/core/user-info.js
+++ src/core/user-info.js
@@ -1,6 +1,10 @@
+const appUrl = 'https://s3.amazonaws.com/kobiton/nightly/'
 const accountLocal = {
   portalUrl: 'http://localhost:3000/',
   apiUrl: 'http://localhost:3000/',
+  appOSXUrl: `${appUrl}kobiton-osx-test.dmg`,
+  appWin32Url: `${appUrl}kobiton-win32-test.exe`,
+  appWin64Url: `${appUrl}kobiton-win64-test.exe`,
   hubUrl: 'localhost:3000',
   emailOrUsername: 'tester',
   password: '123456'
@@ -8,6 +12,9 @@ const accountLocal = {
 const accountTest = {
   portalUrl: 'https://portal-test.kobiton.com',
   apiUrl: 'https://api-test.kobiton.com/',
+  appOSXUrl: `${appUrl}kobiton-osx-test.dmg`,
+  appWin32Url: `${appUrl}kobiton-win32-test.exe`,
+  appWin64Url: `${appUrl}kobiton-win64-test.exe`,
   hubUrl: 'api-test.kobiton.com',
   emailOrUsername: 'api_test1',
   password: 'mario8x@123'
@@ -15,6 +22,9 @@ const accountTest = {
 const accountStaging = {
   portalUrl: 'https://portal-staging.kobiton.com',
   apiUrl: 'https://api-staging.kobiton.com/',
+  appOSXUrl: `${appUrl}kobiton-osx-staging.dmg`,
+  appWin32Url: `${appUrl}kobiton-win32-staging.exe`,
+  appWin64Url: `${appUrl}kobiton-win64-staging.exe`,
   hubUrl: 'api-staging.kobiton.com',
   emailOrUsername: 'staging_test1',
   password: 'mario8x@123'
@@ -22,6 +32,9 @@ const accountStaging = {
 const accountProduction = {
   portalUrl: 'https://portal.kobiton.com',
   apiUrl: 'https://api.kobiton.com/',
+  appOSXUrl: `${appUrl}kobiton-osx.dmg`,
+  appWin32Url: `${appUrl}kobiton-win32.exe`,
+  appWin64Url: `${appUrl}kobiton-win64.exe`,
   hubUrl: 'api.kobiton.com',
   emailOrUsername: 'production_test1',
   password: 'mario8x@123'
