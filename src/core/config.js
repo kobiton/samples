109d1baecfc4824454bdc217d428f373dbbfd599
mario8x <quangxnguyen@kms-technology.com>
2016-08-09 23:10:04 +0700

    Updated e2e test and fixed config issues (#67)

--- src/core/config.js
+++ src/core/config.js
@@ -1,8 +1,9 @@
+
 const _account = {
   portalUrl: process.env.KOBITON_PORTAL_URL || 'https://portal-test.kobiton.com', // We don't need to handle slash for this case
   apiUrl: _removeSlash(process.env.KOBITON_API_URL || 'https://api-test.kobiton.com/') + '/',
   appOSXUrl: process.env.KOBITON_DESKTOP_APP_URL || 'https://s3.amazonaws.com/kobiton/nightly/kobiton-osx-test.dmg',
-  hubUrl: process.env.KOBITON_HUB_HOST || 'api-test.kobiton.com:443',
+  hubHostName: process.env.KOBITON_HUB_HOSTNAME || 'api-test.kobiton.com',
   emailOrUsername: process.env.USERNAME_1 || 'api_test4',
   password: process.env.PASSWORD_1 || 'mario8x@123',
   emailOrUsername2: process.env.USERNAME_2 || 'tester',
