3a18916f969fa13d01a3d213760d8c814108b75d
taynguyen <nguyenvanletay@gmail.com>
2017-03-17 17:54:54 +0700

    Fix issue report data was write in array. (#114)

--- src/framework/common/metrics/db/schema/testcase.js
+++ src/framework/common/metrics/db/schema/testcase.js
@@ -2,17 +2,17 @@ import mongoose from 'mongoose'
 
 export function createTestCaseSchema() {
   const schema = new mongoose.Schema({
-    reportName: {type: [String], index: true},
-    testCaseName: {type: [String], index: true},
+    reportName: {type: String, index: true},
+    testCaseName: {type: String, index: true},
     specHash: String,
     file: String,
-    start: {type: [Date], index: true},
+    start: {type: Date, index: true},
     duration: Number,
-    state: {type: [String], index: true},
+    state: {type: String, index: true},
     parents: Array,
-    metadata: {type: [mongoose.Schema.Types.Mixed], index: true},
+    metadata: {type: mongoose.Schema.Types.Mixed, index: true},
     runner: mongoose.Schema.Types.Mixed,
-    error: {type: [mongoose.Schema.Types.Mixed], index: true}
+    error: {type: mongoose.Schema.Types.Mixed, index: true}
   })
 
   return mongoose.model('TestCase', schema)
