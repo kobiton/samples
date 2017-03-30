b4b138c5993659f3341894c22f16fc0ff62e63bc
taynguyen <nguyenvanletay@gmail.com>
2017-03-30 14:24:22 +0700

    Fix issue when add large data to a indexed field. (#125)

--- src/framework/common/metrics/db/schema/testcase.js
+++ src/framework/common/metrics/db/schema/testcase.js
@@ -9,10 +9,19 @@ export function createTestCaseSchema() {
     start: {type: Date, index: true},
     duration: Number,
     state: {type: String, index: true},
+    environment: {type: String, index: true},
     parents: Array,
     metadata: {type: mongoose.Schema.Types.Mixed, index: true},
     runner: mongoose.Schema.Types.Mixed,
-    error: {type: mongoose.Schema.Types.Mixed, index: true}
+    error: mongoose.Schema.Types.Mixed
+  })
+  schema.index({
+    'error.message': 'text',
+    'error.stack': 'text'
+  })
+  schema.index({
+    'error.expected': 1,
+    'error.actual': 1
   })
 
   return mongoose.model('TestCase', schema)
