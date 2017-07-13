e30d01a9aacadd61d04bfeec73622759a4ff408b
tuyetngo <tuyetngo@users.noreply.github.com>
2017-07-13 22:18:59 +0700

    Update JSON Wire Protocol (#173)
    * Add 5 verified APIs and edit file test
    
    * Edit format and misspelling

--- src/test/console/appium/web/test-jsonwired-api.js
+++ src/test/console/appium/web/test-jsonwired-api.js
@@ -1,27 +1,39 @@
-import {getOnlineCaps} from '../../../../framework/appium/helper'
+import {debug} from '@kobiton/core-util'
+import {assert} from 'chai'
+import moment from 'moment'
+import Device from '../../../../framework/api/device'
 import config from '../../../../framework/config/test'
-import {executeJsonwiredTest} from '../../../../framework/appium/web'
+import {executeJsonwiredTest} from '../../../../framework/appium/web/index'
+import {convertToDesiredCapabilities} from '../../../../framework/appium/helper'
+
+const timeout = 90000 // milliseconds
+const runLoop = config.longTestSuiteIterationAmount
+const timestamps = moment().format('YYYYMMDDHHmmss')
 
 setTimeout(async () => {
-// Get online capabilities
-  const onlineCaps = await getOnlineCaps({
-    deviceNumbers: config.device.number
-  })
-  const timeout = 30000 // milliseconds
-
-  describe('[appium-web]: jsonewired protocol', async () => {
-
-    onlineCaps.forEach((cap) => {
-      // Generate test suite for each of device
-      describe(`${cap.deviceName} : ${cap.platformVersion} `, async () => {
-        it('should cover jsonwired apis', async () => {
-          await executeJsonwiredTest({desiredCapabilities: cap, timeout})
-        })
+  const neededDevices = await Device.getOnlineDevices()
+  assert.isAtLeast(neededDevices.length, 1, 'Expected at least 1 online devices')
+  describe('[Test Webdriverio apis] : automation practice page', async () => {
+    for (const device of neededDevices) {
+      let id = (device.udid) ? `with udid ${device.udid}` : ''
+      describe(`Test ${device.deviceName}: ${device.platformVersion} ${id}`,
+      async () => {
+        const metadata = {...device} // Clone the object
+        for (let i = 0; i < runLoop; i++) {
+          it(`${timestamps} - Loop ${i + 1}/${runLoop} ${JSON.stringify(metadata)}`,
+          async function () { // Use function instead of arrow function to call skip()
+            if (Device.isOnlineDevice(device)) {
+              const onlineCaps = await convertToDesiredCapabilities(timestamps, [device])
+              await executeJsonwiredTest({desiredCapabilities: onlineCaps[0], timeout})
+            }
+            else {
+              debug.log(`Device ${device.deviceName} is not online to run`)
+              this.skip()
+            }
+          })
+        }
       })
-
-    })
+    }
   })
-
   run()
-
 }, 1000)
