ed4094a2e904ed5fd11844fa1d5c198d07a2e33e
Tay Nguyen <nguyenvanletay@gmail.com>
2017-11-09 16:11:00 +0700

    Add missing models

---
+++ java/src/test/java/com/kobiton/model/Device.java
@@ -0,0 +1,86 @@
+package com.kobiton.model;
+
+/*
+ * Created on Nov 9, 2017, 11:47:27 AM
+ *
+ * Copyright(c) 2017 Kobiton Company, Inc.  All Rights Reserved.
+ * This software is the proprietary information of Kobiton Company.
+ *
+ */
+
+public class Device {
+    private String id;
+    private boolean isBooked;
+    private boolean isOnline;
+    private String modelName;
+    private String deviceName;
+    private String platformName;
+    private String platformVersion;
+    private boolean isFavorite;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isIsBooked() {
+        return isBooked;
+    }
+
+    public void setIsBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
+
+    public boolean isIsOnline() {
+        return isOnline;
+    }
+
+    public void setIsOnline(boolean isOnline) {
+        this.isOnline = isOnline;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getPlatformName() {
+        return platformName;
+    }
+
+    public void setPlatformName(String platformName) {
+        this.platformName = platformName;
+    }
+
+    public String getPlatformVersion() {
+        return platformVersion;
+    }
+
+    public void setPlatformVersion(String platformVersion) {
+        this.platformVersion = platformVersion;
+    }
+
+    public boolean isIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+    
+    
+}
