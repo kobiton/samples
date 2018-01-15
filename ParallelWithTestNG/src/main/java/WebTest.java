b642935f6c51c744e549d9d96b11f242d89593b3
Nghi Thai <nghithai@kms-technology.com>
2018-01-15 17:54:04 +0700

    Uploaded sample project for parallel with testNG

---
+++ ParallelWithTestNG/src/main/java/WebTest.java
@@ -0,0 +1,72 @@
+/**
+ * Created by Nghi Thai on 12/29/17.
+ */
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.ITestContext;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.util.Map;
+
+public class WebTest {
+    public static Map<String,String> testngParams;
+    String kobitonURL;
+    RemoteWebDriver driver = null;
+
+    @BeforeTest
+    @Parameters({"platform"})
+    public void Setup(ITestContext testContext, String platform) throws MalformedURLException {
+        testngParams = testContext.getCurrentXmlTest().getAllParameters();
+        kobitonURL = testngParams.get("kobitonURL");
+        driver = new RemoteWebDriver(new URL(kobitonURL), this.generateDesiredCaps(platform));
+        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void Teardown() {
+        try {
+            if (driver != null)
+                driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testKobitonWebsite() throws MalformedURLException, InterruptedException {
+        driver.get("http://google.com");
+        WebElement searchFieldE = driver.findElementByXPath("//input[@name='q']");
+        searchFieldE.sendKeys("Kobiton");
+        searchFieldE.sendKeys(Keys.ENTER);
+        if (driver.findElementsByXPath("//div/span[text()='Looking for results in English?']").size() != 0) {
+            driver.findElementByXPath("//div/a[text()='Change to English']").click();
+        }
+        String message = driver.findElementByXPath("//a[@href='https://kobiton.com/']").getText();
+        Assert.assertTrue(message.contains("Kobiton"));
+    }
+
+    public static final DesiredCapabilities generateDesiredCaps(String platform) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String browserName = "chrome";
+        String deviceName = "Galaxy";
+        if (platform.equals("iOS")) {
+            browserName = "safari";
+            deviceName = "iPhone";
+        }
+        capabilities.setCapability("sessionName", "Run test on " + platform);
+        capabilities.setCapability("sessionDescription", "This is an example for Web testing");
+        capabilities.setCapability("deviceOrientation", "portrait");
+        capabilities.setCapability("captureScreenshots", true);
+        capabilities.setCapability("deviceGroup", "KOBITON");
+        capabilities.setCapability("platformName", platform);
+        capabilities.setCapability("browserName", browserName);
+        capabilities.setCapability("deviceName", deviceName);
+        return capabilities;
+    }
+
+}
