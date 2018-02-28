e28fa3b99658f55b537075ab8cae05f7eaafed12
Khanh Do <khanhdo.pmp@gmail.com>
2018-02-28 10:44:18 +0700

    Added timeouts (pageLoad, implicit) (#228)

--- src/health-check/scripts/daily-web.js
+++ src/health-check/scripts/daily-web.js
@@ -8,7 +8,9 @@ export default class DailyWebTest {
   async execute(timeStamp, targetDevice, expectedDuration) {
     const desiredCapabilities = this._getCap(timeStamp, targetDevice)
     const browser = webdriverio.remote({desiredCapabilities, ...server})
-    browser.timeouts('script', 5 * 60 * 1000)
+    browser.timeouts('pageLoad', 3 * 60 * 1000)
+    browser.timeouts('script', 3 * 60 * 1000)
+    browser.timeouts('implicit', 3 * 60 * 1000)
     const mailinatorPage = new MailinatorPage(browser, timeout)
     await mailinatorPage.executeTest(expectedDuration)
   }
