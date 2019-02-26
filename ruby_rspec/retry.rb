fb866d721e2e527c7b0c017c9e99ccdcb5c6002c
Minh Bui <minhbui@Administrators-MacBook-Pro.local>
2019-02-26 15:41:05 +0700

    fix the last commit

--- ruby_rspec/retry.rb
+++ ruby_rspec/retry.rb
@@ -2,9 +2,9 @@ require 'rspec/retry'
 require "net/http"
 
 RSpec.configure do |config|
-		config.before(:each) do
-				config.default_retry_count = 3
-				# Only retry when Selenium raises Net::ReadTimeout
-				config.exceptions_to_retry = [Net::ReadTimeout] 
-		end
+	config.before(:each) do
+		config.default_retry_count = 3
+		# Only retry when Selenium raises Net::ReadTimeout
+		config.exceptions_to_retry = [Net::ReadTimeout] 
+	end
 end
