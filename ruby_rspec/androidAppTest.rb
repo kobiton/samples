37759d51faf7d735e052bf1695795f1ed57384e3
Minh Bui <minhbui@Administrators-MacBook-Pro.local>
2019-02-26 17:01:27 +0700

    fix the last commit

--- ruby_rspec/androidAppTest.rb
+++ ruby_rspec/androidAppTest.rb
@@ -5,63 +5,63 @@ require "selenium-webdriver"
 describe "Android App Test" do
 
   before(:all) do
-      caps = DesiredCapabilities.new
-      @driver = Appium::Driver.new({:caps => caps.desired_caps_android_app[:caps], 
-      :appium_lib =>{ :server_url => caps.desired_caps_android_app[:url]}},true).start_driver()
-      @driver.manage.timeouts.implicit_wait = 300
+    caps = DesiredCapabilities.new
+    @driver = Appium::Driver.new({:caps => caps.desired_caps_android_app[:caps], 
+    :appium_lib =>{ :server_url => caps.desired_caps_android_app[:url]}},true).start_driver()
+    @driver.manage.timeouts.implicit_wait = 300
   end
 
   after(:all) do
-      @driver.quit
+    @driver.quit
   end   
 
   first_question = 'Acura MDX'
   second_question = 'Cruise Control'
 
   it "Search questions on Acura Support Community" do
-      puts "Test case 1: it should allow to search some questions on Acura Support Community"
-      @driver.find_element(:xpath, "//*[@resource-id='android:id/home']").click
-      sleep 5
-      @driver.find_element(:xpath, "//*[@text='Car and Truck']").click
-      sleep 5
-      @driver.find_element(:xpath, "//*[@text='Acura']").click
+    puts "Test case 1: it should allow to search some questions on Acura Support Community"
+    @driver.find_element(:xpath, "//*[@resource-id='android:id/home']").click
+    sleep 5
+    @driver.find_element(:xpath, "//*[@text='Car and Truck']").click
+    sleep 5
+    @driver.find_element(:xpath, "//*[@text='Acura']").click
 
-      acura_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=1]").text
-      acura_integra_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=3]").text
-      acura_mdx_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=4]").text
-      acura_rl_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=5]").text
-      acura_tl_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=6]").text
-      acura_tsx_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=7]").text
+    acura_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=1]").text
+    acura_integra_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=3]").text
+    acura_mdx_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=4]").text
+    acura_rl_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=5]").text
+    acura_tl_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=6]").text
+    acura_tsx_text = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/topic_title' and @index=7]").text
 
-      expect(acura_text).to eq('Acura')
-      expect(acura_integra_text).to eq('Acura Integra')
-      expect(acura_mdx_text).to eq('Acura MDX')
-      expect(acura_rl_text).to eq('Acura RL')
-      expect(acura_tl_text).to eq('Acura TL')
-      expect(acura_tsx_text).to eq('Acura TSX')
+    expect(acura_text).to eq('Acura')
+    expect(acura_integra_text).to eq('Acura Integra')
+    expect(acura_mdx_text).to eq('Acura MDX')
+    expect(acura_rl_text).to eq('Acura RL')
+    expect(acura_tl_text).to eq('Acura TL')
+    expect(acura_tsx_text).to eq('Acura TSX')
 
-      @driver.close_app
+    @driver.close_app
   end
 
   it "verify_search_iFixit_on_home_screen" do
-      puts 'Test case 2: it should allow to search iFixit on Home screen'
-      @driver.launch_app
-      sleep 5
-      @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/action_search']").click
-      @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/abs__search_src_text']").send_keys('Macbook Pro 2015')
-      @driver.press_keycode(66);
-      sleep 5
-      first_result = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/search_result_count']").text
-      @driver.find_element(:xpath, "//*[@resource-id='android:id/text1' and @text='Guides']").click
-      @driver.find_element(:xpath, "//*[@resource-id='android:id/text1' and @text='Devices']").click
+    puts 'Test case 2: it should allow to search iFixit on Home screen'
+    @driver.launch_app
+    sleep 5
+    @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/action_search']").click
+    @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/abs__search_src_text']").send_keys('Macbook Pro 2015')
+    @driver.press_keycode(66);
+    sleep 5
+    first_result = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/search_result_count']").text
+    @driver.find_element(:xpath, "//*[@resource-id='android:id/text1' and @text='Guides']").click
+    @driver.find_element(:xpath, "//*[@resource-id='android:id/text1' and @text='Devices']").click
 
-      second_result = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/search_result_count']").text
+    second_result = @driver.find_element(:xpath, "//*[@resource-id='com.dozuki.ifixit:id/search_result_count']").text
 
-      first_result = first_result.gsub(/[^0-9]/, '')
-      second_result = second_result.gsub(/[^0-9]/, '')
+    first_result = first_result.gsub(/[^0-9]/, '')
+    second_result = second_result.gsub(/[^0-9]/, '')
 
-      expect(first_result.to_i).to be >= 72
-      expect(second_result.to_i).to be >= 5
+    expect(first_result.to_i).to be >= 72
+    expect(second_result.to_i).to be >= 5
   end
 end
 
--- ruby_rspec/iOSAppTest.rb
+++ ruby_rspec/iOSAppTest.rb
@@ -4,61 +4,61 @@ require "selenium-webdriver"
 
 describe "iOS App Test" do
 
-    before(:all) do
-        caps = DesiredCapabilities.new
-        @driver = Appium::Driver.new({:caps => caps.desired_caps_ios_app[:caps],
-        :appium_lib =>{ :server_url => caps.desired_caps_ios_app[:url]}},true).start_driver()
-        @driver.manage.timeouts.implicit_wait = 300
-    end
+  before(:all) do
+    caps = DesiredCapabilities.new
+    @driver = Appium::Driver.new({:caps => caps.desired_caps_ios_app[:caps],
+    :appium_lib =>{ :server_url => caps.desired_caps_ios_app[:url]}},true).start_driver()
+    @driver.manage.timeouts.implicit_wait = 300
+  end
 
-    after(:all) do
-        @driver.quit
-    end 
+  after(:all) do
+    @driver.quit
+  end 
 
-    it 'Navigate to some devices on Acura Support Community' do
-        puts "Test case 1: it should allow to search some questions on Acura Support Community"
-        @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='START A REPAIR']").click
-        sleep 10
-        @driver.find_element(:xpath, "//*[@name='Car and Truck']").click
-        sleep 10
-        @driver.find_element(:xpath, "//*[@name='Acura']").click
+  it 'Navigate to some devices on Acura Support Community' do
+    puts "Test case 1: it should allow to search some questions on Acura Support Community"
+    @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='START A REPAIR']").click
+    sleep 10
+    @driver.find_element(:xpath, "//*[@name='Car and Truck']").click
+    sleep 10
+    @driver.find_element(:xpath, "//*[@name='Acura']").click
 
-        acura_text = @driver.find_element(:xpath, "//XCUIElementTypeNavigationBar").attribute("name")
+    acura_text = @driver.find_element(:xpath, "//XCUIElementTypeNavigationBar").attribute("name")
 
-        has_acura_integra = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura Integra']").displayed?
-        has_acura_mdx = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura MDX']").displayed?
-        has_acura_rl = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura RL']").displayed?
-        has_acura_tl = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura TL']").displayed?
-        has_acura_tsx = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura TSX']").displayed?
+    has_acura_integra = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura Integra']").displayed?
+    has_acura_mdx = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura MDX']").displayed?
+    has_acura_rl = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura RL']").displayed?
+    has_acura_tl = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura TL']").displayed?
+    has_acura_tsx = @driver.find_element(:xpath, "//XCUIElementTypeStaticText[@name='Acura TSX']").displayed?
 
-        expect(acura_text).to eq('Acura')
-        expect(has_acura_integra).to eq(true)
-        expect(has_acura_mdx).to eq(true)
-        expect(has_acura_rl).to eq(true)
-        expect(has_acura_tl).to eq(true)
-        expect(has_acura_tsx).to eq(true)
-        @driver.close_app
-    end
+    expect(acura_text).to eq('Acura')
+    expect(has_acura_integra).to eq(true)
+    expect(has_acura_mdx).to eq(true)
+    expect(has_acura_rl).to eq(true)
+    expect(has_acura_tl).to eq(true)
+    expect(has_acura_tsx).to eq(true)
+    @driver.close_app
+  end
 
-    it 'Search iFixit on Home screen' do
-        puts "Test case 2: it should allow to search iFixit on Home screen"
-        @driver.launch_app
-        sleep 10
-        @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='START A REPAIR']").click
-        sleep 10
+  it 'Search iFixit on Home screen' do
+    puts "Test case 2: it should allow to search iFixit on Home screen"
+    @driver.launch_app
+    sleep 10
+    @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='START A REPAIR']").click
+    sleep 10
 
-        @driver.find_element(:xpath, "//XCUIElementTypeSearchField[@name='Search']").send_keys('Macbook Pro 2015')
-        first_result = @driver.find_elements(:xpath, "//XCUIElementTypeStaticText[contains(@label,'MacBook Pro')]")
-        @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='Cancel']").click
+    @driver.find_element(:xpath, "//XCUIElementTypeSearchField[@name='Search']").send_keys('Macbook Pro 2015')
+    first_result = @driver.find_elements(:xpath, "//XCUIElementTypeStaticText[contains(@label,'MacBook Pro')]")
+    @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='Cancel']").click
 
-        @driver.find_element(:xpath, "//*[@name='Search']").click
-        @driver.find_element(:xpath, "//XCUIElementTypeSearchField[@name='Search']").send_keys('Acura')
-        @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='Categories']").click
+    @driver.find_element(:xpath, "//*[@name='Search']").click
+    @driver.find_element(:xpath, "//XCUIElementTypeSearchField[@name='Search']").send_keys('Acura')
+    @driver.find_element(:xpath, "//XCUIElementTypeButton[@name='Categories']").click
 
-        second_result = @driver.find_elements(:xpath, "//XCUIElementTypeStaticText[contains(@label,'Acura')]")
+    second_result = @driver.find_elements(:xpath, "//XCUIElementTypeStaticText[contains(@label,'Acura')]")
 
-        expect(first_result.size).to be >= 39
-        expect(second_result.size).to be >= 20
-    end
+    expect(first_result.size).to be >= 39
+    expect(second_result.size).to be >= 20
+  end
 end
 
