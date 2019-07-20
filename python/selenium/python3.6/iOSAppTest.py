51bb6880f77bb238d3633c6f922292e2624b9166
HienNguyenT <hientnguyen+2002@kobiton.com>
2019-07-20 17:58:23 +0700

    update

--- python/selenium/python3.6/iOSAppTest.py
+++ python/selenium/python3.6/iOSAppTest.py
@@ -1,5 +1,7 @@
 from appium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import unittest
 import time
 import urllib3
