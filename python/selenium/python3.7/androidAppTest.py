f7aad63c859bbfa7f2570fd1c173b5813d8d4090
HienNguyenT <hientnguyen+2002@kobiton.com>
2019-07-23 09:34:02 +0700

    update

--- python/selenium/python3.7/androidAppTest.py
+++ python/selenium/python3.7/androidAppTest.py
@@ -2,7 +2,6 @@ from appium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.remote.remote_connection import RemoteConnection
 import unittest
 import re
 import time
