4d7e94b62760968ac56bf4ff02b191aaf3afaee0
Tan Le <handoiqn2014z@gmail.com>
2019-08-29 10:04:10 +0700

    Update

--- kobiton-rest-api/upload-app-to-kobiton-apps-repo/java/main.java
+++ kobiton-rest-api/upload-app-to-kobiton-apps-repo/java/main.java
@@ -7,20 +7,22 @@ import java.net.URLConnection;
 
 public class Main {
 
-    static  String filePath ="com.dozuki.ifixit.apk";
-    static  String fileName ="iFixit.apk";
+    static String filePath = "com.dozuki.ifixit.apk";
+    static String fileName = "iFixit.apk";
 
     public static void main(String[] args) {
 
         System.out.println("If you want to use your app at local, please comment the downloadFile() method");
-        Common.downloadFile("https://s3-ap-southeast-1.amazonaws.com/kobiton-devvn/apps-test/demo/com.dozuki.ifixit.apk", "com.dozuki.ifixit.apk");
+        Common.downloadFile(
+                "https://s3-ap-southeast-1.amazonaws.com/kobiton-devvn/apps-test/demo/com.dozuki.ifixit.apk",
+                "com.dozuki.ifixit.apk");
         System.out.println("Step 1: Generate Upload URL");
 
         File file = new File(filePath);
         filePath = file.getAbsolutePath();
 
         String jsonData = Common.generateUploadURL(fileName);
-        JsonObject jsonObject =(JsonObject) (new JsonParser()).parse(jsonData);
+        JsonObject jsonObject = (JsonObject) (new JsonParser()).parse(jsonData);
 
         String appPath = jsonObject.getAsJsonPrimitive("appPath").getAsString();
         String appURL = jsonObject.getAsJsonPrimitive("url").getAsString();
@@ -31,7 +33,7 @@ public class Main {
 
         System.out.println("Step 3: Create an app or app version");
         String appResult = Common.createAnAppOrVersion(fileName, appPath);
-        jsonObject =(JsonObject) (new JsonParser()).parse(appResult);
+        jsonObject = (JsonObject) (new JsonParser()).parse(appResult);
         int appId = jsonObject.getAsJsonPrimitive("appId").getAsInt();
         int versionId = jsonObject.getAsJsonPrimitive("versionId").getAsInt();
 
