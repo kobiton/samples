80fac89b79aded135d1af02b4e917244a7edeea5
Khanh Do <khanhdo.pmp@gmail.com>
2020-04-24 22:38:07 +0700

    Support parallel tests and wait a specific device

---
+++ java/java_testng_junit/src/main/java/kobiton/com/configs/CheckDeviceStatus.java
@@ -0,0 +1,74 @@
+package kobiton.com.configs;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.*;
+
+public class CheckDeviceStatus {
+
+    public static JSONObject filterDeviceInfo(JSONObject device) {
+        JSONObject filteredDeviceInfo = new JSONObject();
+        filteredDeviceInfo.put("id", device.get("id"));
+        filteredDeviceInfo.put("udid", device.get("udid"));
+        filteredDeviceInfo.put("isBooked", device.get("isBooked"));
+        filteredDeviceInfo.put("modelName", device.get("modelName"));
+        filteredDeviceInfo.put("deviceName", device.get("deviceName"));
+        filteredDeviceInfo.put("platformName", device.get("platformName"));
+        filteredDeviceInfo.put("platformVersion", device.get("platformVersion"));
+        return filteredDeviceInfo;
+    }
+
+    public static JSONArray getOnlineDevices(String username, String apiKey, String deviceGroup) {
+
+        try {
+            URL obj = new URL("https://api.kobiton.com/v1/devices?isOnline=true");
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Authorization", Configs.generateBasicAuth(username, apiKey));
+            int responseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            if (responseCode != 200) {
+                System.exit(1);
+            }
+
+            JSONObject deviceList = new JSONObject(response.toString());
+            JSONArray cloudDevices = deviceList.getJSONArray(deviceGroup);
+            JSONArray filteredDeviceInfo = new JSONArray();
+
+            for (int i = 0; i < cloudDevices.length(); i++) {
+                filteredDeviceInfo.put(filterDeviceInfo(cloudDevices.getJSONObject(i)));
+            }
+
+            return filteredDeviceInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static boolean fitlterDeviceOnlineByUDID(JSONArray devices, String udid) {
+        boolean flag = false;
+        if (udid.equals("*") && devices.length() > 0) return true;
+        for (int i = 0; i < devices.length(); i++) {
+            String data = devices.getJSONObject(i).getString("udid");
+            if (data.equals(udid)) {
+                System.out.println(data);
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+}
---
+++ java/java_testng_junit/src/main/java/kobiton/com/configs/Utils.java
@@ -0,0 +1,31 @@
+package kobiton.com.configs;
+
+import org.json.JSONArray;
+
+public class Utils {
+    public static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean waitForDevice(String deviceGroup, String udid) {
+        int loop = 4;
+        int start = 0;
+        boolean flag = false;
+        while(start < loop) {
+            JSONArray onlineDevices = CheckDeviceStatus.getOnlineDevices(Configs.USERNAME, Configs.ACCESS_KEY, deviceGroup);
+            if(CheckDeviceStatus.fitlterDeviceOnlineByUDID(onlineDevices, udid) == true) {
+                flag = true;
+                break;
+            } else {
+                sleep(1);
+                start++;
+            }
+        }
+
+        return flag;
+    }
+}
