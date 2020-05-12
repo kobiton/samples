2b38af013f320714271e572cc6bab86364549298
Huyen Do <62511307+hyndole@users.noreply.github.com>
2020-05-12 14:43:16 +0700

    Update AndroidAppTest.java

--- java/DeepLink/src/test/java/tests/AndroidAppTest.java
+++ java/DeepLink/src/test/java/tests/AndroidAppTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import com.google.inject.internal.util.ImmutableMap;
@@ -14,10 +13,10 @@ import configs.Configs;
 
 public class AndroidAppTest {
 
-    private static AndroidDriver<WebElement> driver = null;
+    AndroidDriver<WebElement> driver = null;
 
     @BeforeTest
-    public void Setup() throws MalformedURLException {
+    public void Setup() {
         driver = new AndroidDriver<WebElement>(Configs.kobitonServerUrl(), Configs.desiredCapabilitiesAndroidApp());
         driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
     }
