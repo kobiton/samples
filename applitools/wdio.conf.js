2184ed223d8ba0dce868aced5d243f57ccb4ca1d
oanhpham <oanhpham@kobiton.com>
2020-06-03 11:26:11 +0700

    remove package-lock.json

--- applitools/wdio.conf.js
+++ applitools/wdio.conf.js
@@ -1,36 +1,35 @@
 exports.config = {
-    user: process.env.KOBITON_USERNAME,
-    key: process.env.KOBITON_API_KEY,
-    framework: 'mocha',
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 600000
+  user: process.env.KOBITON_USERNAME,
+  key: process.env.KOBITON_API_KEY,
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 600000
     },
-    sync: false,
-    logLevel: 'silent',
-    specs: [
-        './test/**/*.js'
-    ],  
-    capabilities: [{
-        sessionName:        'Android web test',
-        sessionDescription: 'This is an example for Android web',
-        deviceOrientation:  'portrait',
-        captureScreenshots: true,
-        browserName:        'chrome',
-        deviceGroup:        'KOBITON',
-        deviceName:         '*',
-        platformVersion:    '*',
-        platformName:       'Android'
-    }],
-    deprecationWarnings: true,
-    bail: 0,
-    baseUrl: 'https://portal.kobiton.com',
-    hostname: 'api.kobiton.com',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-    reporters: ['spec'],
-    port: 443,
-    maxInstances: 1,
+  sync: false,
+  logLevel: 'silent',
+  specs: [
+    './test/**/*.js'
+  ],  
+  capabilities: [{
+    sessionName:        'Android web test',
+    sessionDescription: 'This is an example for Android web',
+    deviceOrientation:  'portrait',
+    captureScreenshots: true,
+    browserName:        'chrome',
+    deviceGroup:        'KOBITON',
+    deviceName:         '*',
+    platformVersion:    '*',
+    platformName:       'Android'
+  }],
+  deprecationWarnings: true,
+  bail: 0,
+  baseUrl: 'https://portal.kobiton.com',
+  hostname: 'api.kobiton.com',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  reporters: ['spec'],
+  port: 443,
+  maxInstances: 1,
 }
-  
