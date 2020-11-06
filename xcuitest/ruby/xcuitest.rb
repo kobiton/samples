1cde309463de5b78552854db70a145684f688846
Trương Thế Luân <truongluan100695@gmail.com>
2020-11-06 14:07:31 +0700

    Upload Public Sample Test
    Upload Public Sample Test

--- xcuitest/ruby/xcuitest.rb
+++ xcuitest/ruby/xcuitest.rb
@@ -24,12 +24,12 @@ configuration = {
     :deviceName =>         '*',
     :platformVersion =>    '*',  
     :deviceGroup =>        'KOBITON',
-    :app =>                'https://kobiton-devvn.s3.ap-southeast-1.amazonaws.com/test-runner/users/6/XCUITestSample-cf0cae10-1f0e-11eb-bc6c-e3417e3453b3.ipa?AWSAccessKeyId=AKIAYOIAYUIRQXR7QYH2&Expires=1604586603&Signature=OrqPAP8KJWapIuStzXmHwS28fNo%3D',
-    :testRunner =>         'https://kobiton-devvn.s3.ap-southeast-1.amazonaws.com/test-runner/users/6/XCUITestSampleUITestRunner-e3ef3500-1f0e-11eb-b45a-478d7efceb5e.ipa?AWSAccessKeyId=AKIAYOIAYUIRQXR7QYH2&Expires=1604586641&Signature=DDhYizx%2FdKMZyMe%2F5QEXx5gLoGs%3D', 
+    :app =>                'https://kobiton-devvn.s3-ap-southeast-1.amazonaws.com/apps-test/XCUITestSample.ipa',
+    :testRunner =>         'https://kobiton-devvn.s3-ap-southeast-1.amazonaws.com/apps-test/XCUITestSampleUITestRunner.ipa', 
     :testFramework =>      'XCUITEST',
     :sessionTimeout =>     30,
-    :tests =>               []
-    :testPlan =>            'https://kobiton-devvn.s3.ap-southeast-1.amazonaws.com/test-plan/users/6/sample-fe1f27f0-1f0e-11eb-964e-0f0d0f83dd07.xctestplan?AWSAccessKeyId=AKIAYOIAYUIRQXR7QYH2&Expires=1604586682&Signature=WESzuwFm55OqM4R4%2B8Zq0rD6TnA%3D'
+    :tests =>               [],
+    :testPlan =>           'https://kobiton-devvn.s3-ap-southeast-1.amazonaws.com/apps-test/sample.xctestplan'
   }
 }
 
