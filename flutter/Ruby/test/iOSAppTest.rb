188913f6c9245292a529fade6450f513aea3d56a
Huyen Do <62511307+hyndole@users.noreply.github.com>
2021-06-01 15:44:09 +0700

    Update iOSAppTest.rb

--- flutter/Ruby/test/iOSAppTest.rb
+++ flutter/Ruby/test/iOSAppTest.rb
@@ -6,9 +6,9 @@ require 'appium_lib'
 require 'minitest/autorun'
 require './configs.rb'
 require 'appium_lib_core/common/device/context.rb'
+
 class ExampleTests < Minitest::Test
   include ::Appium::Flutter::Finder
-  
 
   def test_run_example_ios_scenario
     desire_cap = DesiredCapabilities.new
@@ -27,20 +27,16 @@ class ExampleTests < Minitest::Test
 
     @wait.until { @driver.set_context 'NATIVE_APP' }
    
-
     @wait.until { @driver.save_screenshot('./native-screenshot.png') }
 
     @wait.until { @driver.set_context 'FLUTTER'}
    
-
     @wait.until { buttonFinder.click()}
-    
 
     @wait.until { @driver.save_screenshot('./flutter-screenshot.png') }
 
     @wait.until {buttonFinder.click() }
     
-
     @wait.until {@driver.save_screenshot('./flutter-tab.png') }
 
     puts counterTextFinder.text()
