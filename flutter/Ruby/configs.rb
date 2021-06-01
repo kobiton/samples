c4b1af0be6fbda541480f37645440b20e9fe1486
Huyen Do <62511307+hyndole@users.noreply.github.com>
2021-06-01 15:41:05 +0700

    Update configs.rb

--- flutter/Ruby/configs.rb
+++ flutter/Ruby/configs.rb
@@ -1,8 +1,7 @@
-USER_NAME = "kobitonadmin"
-API_KEY = "c6d29894-9633-41ec-8346-dd77029998ab"
+USER_NAME = ""
+API_KEY = ""
 
 class DesiredCapabilities
- 
  def desired_caps_android_app
   desired_caps = {
     :caps => {
@@ -12,23 +11,17 @@ class DesiredCapabilities
       :noReset =>            true,
       :fullReset =>          false,
       :captureScreenshots => true,
-      
-      # The given group is used for finding devices and the created session will be visible for all members within the group.
-      :groupId =>            583, # Group: Im Group
-      :deviceGroup =>        'ORGANIZATION',
-      
-      :deviceName =>         'Moto X Play',
-    
+      :deviceGroup =>        'KOBITON',
+      :deviceName =>         '*',
       :tagName =>            '',
       :platformName =>       'Android',
-      :platformVersion =>    '7.1.1',
+      :platformVersion =>    '*',
       :retry_backoff_time => 5000,
       :automationName => 'Flutter',
       :app => 'https://kobiton-devvn.s3-ap-southeast-1.amazonaws.com/apps-test/demo/app-debug-all.apk'
-
     },
     :appium_lib => {
-      :server_url => "https://#{USER_NAME}:#{API_KEY}@api-test.kobiton.com/wd/hub",
+      :server_url => "https://#{USER_NAME}:#{API_KEY}@api.kobiton.com/wd/hub",
       :wait_timeout => 20,
       :wait_interval => 1
     }.freeze
@@ -43,9 +36,8 @@ class DesiredCapabilities
        :sessionDescription => 'This is an example for iOS App testing', 
        :deviceOrientation =>  'portrait',  
        :captureScreenshots => true, 
-       :groupId =>            583,
-       :deviceGroup =>        'ORGANIZATION', 
-       :deviceName =>         'iPhone 7',
+       :deviceGroup =>        'KOBITON', 
+       :deviceName =>         '*',
        :platformName =>       'iOS',
        :retry_backoff_time => 5000,
        :automationName => 'Flutter',
@@ -59,5 +51,4 @@ class DesiredCapabilities
    }.freeze
    return desired_caps
  end
- 
 end
